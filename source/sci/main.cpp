880a2b5f5cfeb9e0d13d7e6458dc4a70782bf42d
Jakub <jakuborzechowski17@gmail.com>
2018-12-12 16:56:47 +0100

    sci

--- source/sci/main.cpp
+++ source/sci/main.cpp
@@ -1,10 +1,43 @@
 #include <cstdlib>
 #include <sci/Application.hpp>
+#include <SDL2/sdl.h>
+#include <iostream>
+
+using namespace std;
 
 
 int main(int argc, char* argv[])
 {
     Application::create_instance();
+
+
+	SDL_Window*Window=SDL_CreateWindow("sciokno",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE  );
+
+	int s = 0;
+	while (s == 0)
+	{
+		SDL_Event zdarzenie;
+		
+
+		while (SDL_PollEvent(&zdarzenie)) 
+		{
+			switch (zdarzenie.type)
+
+				case  SDL_KEYDOWN:
+				case  SDL_KEYUP:
+
+					cout << zdarzenie.key.timestamp;
+					cout << zdarzenie.key.keysym.scancode;
+
+						
+
+			    break;
+ 
+				
+		}
+
+
+	}
 
     return 0;
 }
